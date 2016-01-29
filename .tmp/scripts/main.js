2134917958767a4de74c5c057739ce5586f2bce1
Axel Glorieux <axel.glorieux@gmail.com>
2016-01-29 22:07:08 +0100

    updated pages

--- .tmp/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// jshint devel:true
-'use strict';
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="popover"]').popover();
-});
-//# sourceMappingURL=main.js.map
